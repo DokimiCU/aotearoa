3316e6e0cb96348ac94ddfe607f9e5471e2c2189
DokimiCU <DokimiCU@gmail.com>
2018-09-20 00:08:52 +1200

    andesite in default stone

--- ores.lua
+++ ores.lua
@@ -73,6 +73,30 @@ minetest.register_ore({
 	},
 })
 
+
+-- andesite into deep stone
+minetest.register_ore({
+	ore_type        = "blob",
+	ore             = "aotearoa:andesite",
+	wherein         = {
+		"default:stone",
+	 },
+	clust_scarcity  = 16 * 16 * 16,
+	clust_size      = 15,
+	y_min           = -31000,
+	y_max           = 31000,
+	noise_threshold = 0.0,
+	noise_params    = {
+		offset = 0,
+		scale = 1,
+		spread = {x = 15, y = 15, z = 15},
+		seed = -11117,
+		octaves = 1,
+		persist = 0.0
+	},
+})
+
+
 -- limestone into sandstone
 minetest.register_ore({
 	ore_type        = "blob",
