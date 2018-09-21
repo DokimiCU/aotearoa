df265fdedab496009b1983e6281fa4d9f1d8b8b0
DokimiCU <DokimiCU@gmail.com>
2018-09-21 20:23:18 +1200

    greywacke in default and schist

--- ores.lua
+++ ores.lua
@@ -96,6 +96,30 @@ minetest.register_ore({
 	},
 })
 
+
+-- greywacke into deep stone and schist
+minetest.register_ore({
+	ore_type        = "blob",
+	ore             = "aotearoa:greywacke",
+	wherein         = {
+		"default:stone",
+		"aotearoa:schist",
+	 },
+	clust_scarcity  = 16 * 16 * 16,
+	clust_size      = 15,
+	y_min           = -31000,
+	y_max           = 31000,
+	noise_threshold = 0.0,
+	noise_params    = {
+		offset = 0,
+		scale = 1,
+		spread = {x = 15, y = 15, z = 15},
+		seed = -145366,
+		octaves = 1,
+		persist = 0.0
+	},
+})
+
 -- andesite into deep stone
 minetest.register_ore({
 	ore_type        = "blob",
