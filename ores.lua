adcd9598c8708cb0ced8a29e6a1960a7bf6e00b0
DokimiCU <DokimiCU@gmail.com>
2018-09-23 11:43:13 +1200

    iron sand in volcanic sand

--- ores.lua
+++ ores.lua
@@ -322,6 +322,28 @@ minetest.register_ore({
 })
 
 
+-- Small bits of iron-sand in Volcanic sand
+minetest.register_ore({
+	ore_type        = "blob",
+	ore             = "aotearoa:iron_sand",
+	wherein         = {
+		"aotearoa:volcanic_sand",
+	 },
+	clust_scarcity  = 16 * 16 * 16,
+	clust_size      = 2,
+	y_min           = -31000,
+	y_max           = 31000,
+	noise_threshold = 0.0,
+	noise_params    = {
+		offset = 0.5,
+		scale = 0.01,
+		spread = {x = 5, y = 5, z = 5},
+		seed = 86811112,
+		octaves = 1,
+		persist = 0.2
+	},
+})
+
 
 -----------------------------------------
 --SPECIAL FEATURES & MINERALS
