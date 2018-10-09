d8b2e4c69d0e53eb9fdf8b51ad5a62b2073e208b
DokimiCU <DokimiCU@gmail.com>
2018-10-09 22:04:07 +1300

    add pipi

--- node_defs.lua
+++ node_defs.lua
@@ -805,3 +805,68 @@ minetest.register_craft({
 	recipe = "aotearoa:raupo_flower",
 	burntime = 1,
 })
+
+
+
+
+-----------------------------
+-- Pipi (shellfish)
+
+minetest.register_node("aotearoa:pipi", {
+	description = "Pipi (Paphies australis)",
+	drawtype = "plantlike",
+	paramtype = "light",
+	visual_scale = 0.6,
+	tiles = {"aotearoa_pipi.png"},
+	inventory_image = "aotearoa_pipi.png",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.1, -0.5, -0.1, 0.1, -0.2, 0.1}
+	},
+	groups = {fleshy = 3, dig_immediate = 3},
+
+	sounds = default.node_sound_gravel_defaults(),
+	--rebury shellfish
+	on_timer = function(pos)
+		local below = {x = pos.x, y = pos.y - 1, z = pos.z}
+			if minetest.get_node(below).name == "default:sand" then
+				minetest.set_node(pos, {name = "air"})
+				minetest.set_node(below, {name = "aotearoa:sand_with_pipi"})
+			elseif minetest.get_node(below).name == "aotearoa:iron_sand" then
+				minetest.set_node(pos, {name = "air"})
+				minetest.set_node(below, {name = "aotearoa:iron_sand_with_pipi"})
+			end
+	end,
+	on_construct = function(pos)
+		minetest.get_node_timer(pos):start(math.random(3, 5))
+	end,
+	--eat and get shells
+	on_use = minetest.item_eat(3,"aotearoa:seashells")
+})
+
+--pipi in sand
+minetest.register_node("aotearoa:sand_with_pipi", {
+	description = "Sand with Pipi",
+	tiles = {"aotearoa_sand_with_pipi.png"},
+	groups = {crumbly = 3, falling_node = 1, sand = 1},
+	sounds = default.node_sound_sand_defaults(),
+	--dig up the shellfish
+	on_punch = function(pos, node, puncher)
+		puncher:get_inventory():add_item('main', "aotearoa:pipi")
+		minetest.set_node(pos, {name = "default:sand"})
+	end,
+})
+
+minetest.register_node("aotearoa:iron_sand_with_pipi", {
+	description = "Iron-sand with Pipi",
+	tiles = {"aotearoa_iron_sand_with_pipi.png"},
+	groups = {crumbly = 3, falling_node = 1, sand = 1},
+	sounds = default.node_sound_sand_defaults(),
+	--dig up the shellfish
+	on_punch = function(pos, node, puncher)
+		puncher:get_inventory():add_item('main', "aotearoa:pipi")
+		minetest.set_node(pos, {name = "aotearoa:iron_sand"})
+	end,
+})
