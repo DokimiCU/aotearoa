bb1b8c82859d7d1e3045c806c2fa8e56905da44b
DokimiCU <DokimiCU@gmail.com>
2018-10-10 17:25:40 +1300

    animation for mud, muddy footstep sounds

--- blocks.lua
+++ blocks.lua
@@ -113,7 +113,8 @@ minetest.register_node("aotearoa:peat", {
 		}
 	},
 	sounds = default.node_sound_dirt_defaults({
-		footstep = {name = "default_water_footstep", gain = 0.02},
+		footstep = {name = "aotearoa_mud", gain = 0.4},
+		dug = {name = "aotearoa_mud", gain = 0.4},
 	}),
 })
 
@@ -147,7 +148,8 @@ for i in ipairs(aotearoa.peatlist) do
 				}
 			},
 			sounds = default.node_sound_dirt_defaults({
-				footstep = {name = "default_water_footstep", gain = 0.2},
+				footstep = {name = "aotearoa_mud", gain = 0.4},
+				dug = {name = "aotearoa_mud", gain = 0.4},
 			}),
 		})
 
@@ -185,8 +187,17 @@ stairs.register_stair_and_slab(
 minetest.register_node("aotearoa:mud_sinking", {
 	description = "Sinking Mud",
 	tiles = {
-		"aotearoa_mud.png",
+		{
+			name = "aotearoa_mud_animated.png",
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
 	},
+	paramtype = "light",
 	walkable = false,
 	--diggable = false,
 	--pointable = false,
@@ -195,8 +206,9 @@ minetest.register_node("aotearoa:mud_sinking", {
 	post_effect_color = {a = 250, r = 20, g = 20, b = 20},
 	groups = {crumbly = 3, puts_out_fire = 1, cools_lava = 1},
 	drop = "aotearoa:silt",
-		sounds = default.node_sound_dirt_defaults({
+	sounds = default.node_sound_dirt_defaults({
 		footstep = {name = "default_water_footstep", gain = 0.2},
+		dug = {name = "aotearoa_mud", gain = 0.4},
 	}),
 })
 
@@ -213,7 +225,8 @@ minetest.register_node("aotearoa:mud", {
 	groups = {crumbly = 3, puts_out_fire = 1},
 	drop = "aotearoa:silt",
 	sounds = default.node_sound_dirt_defaults({
-		footstep = {name = "default_water_footstep", gain = 0.2},
+		footstep = {name = "aotearoa_mud", gain = 0.4},
+		dug = {name = "aotearoa_mud", gain = 0.4},
 	}),
 })
 
@@ -262,6 +275,10 @@ minetest.register_node("aotearoa:boiling_mud_source", {
 	damage_per_second = 1,
 	post_effect_color = {a = 200, r = 73, g = 64, b = 55},
 	groups = {liquid = 2},
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name = "aotearoa_mud", gain = 0.4},
+		dug = {name = "aotearoa_mud", gain = 0.4},
+	}),
 })
 
 minetest.register_node("aotearoa:boiling_mud_flowing", {
@@ -308,6 +325,10 @@ minetest.register_node("aotearoa:boiling_mud_flowing", {
 	damage_per_second = 1,
 	post_effect_color = {a = 200, r = 73, g = 64, b = 55},
 	groups = {liquid = 2,	not_in_creative_inventory = 1},
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name = "aotearoa_mud", gain = 0.4},
+		dug = {name = "aotearoa_mud", gain = 0.4},
+	}),
 })
 
 
