c5e2bdb9c0db0cb15ab6391799d8b10cc6eb0029
DokimiCU <DokimiCU@gmail.com>
2018-10-10 17:23:42 +1300

    spawn cockles

--- spawn_aquatic.lua
+++ spawn_aquatic.lua
@@ -49,3 +49,30 @@ minetest.register_ore({
       "iron_sand_beach",
     },
 })
+
+
+
+-------------------------
+--cockle shellfish beds
+minetest.register_ore({
+		ore_type        = "puff",
+		ore             = "aotearoa:mud_with_cockles",
+		wherein         = {"aotearoa:mud"},
+		clust_scarcity  = 16 * 16 * 16,
+		clust_size      = 3,
+		y_min           = -7,
+		y_max           = 0,
+		noise_threshold = 0.19,
+		noise_params    = {
+			offset = 0,
+			scale = 0.8,
+			spread = {x = 32, y = 32, z = 32},
+			seed = 4607,
+			octaves = 2,
+			persist = 0.4
+		},
+		biomes = {
+      "northern_estuary",
+      "southern_estuary",
+    },
+})
